961e879ab8b9ce94fc19d5b377cc11cf1b06e270
Aaron <Aarontpze@yahoo.com>
2018-11-15 15:48:05 -0800

    Initial commit of projects for Nucleo/custom BallRail board

---
+++ BallRail/Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.d
@@ -0,0 +1,107 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_dma_ex.c \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Inc/stm32l4xx_hal_conf.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Inc/main.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cm4.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ /home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Inc/stm32l4xx_hal_conf.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Inc/main.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l476xx.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+
+/home/aaron/Documents/School/ME507/BallRail/BallRail/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
